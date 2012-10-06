fc5b6bad4b9c556082d4c5a952321f20de907628
Philip Ludington <mrphil@mrphilgames.com>
2012-10-06 18:58:59 -0400

    003 & 004

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public int moveSpeed;
+    public int rotationSpeed;
+
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject gameObject = GameObject.FindGameObjectWithTag("Player");
+
+        target = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+
+        // Look at target/player
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+            Quaternion.LookRotation(target.position - myTransform.position),
+            rotationSpeed * Time.deltaTime);
+
+        // Move towards target/player
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+    }
+}
