83ab012d47946c60a796770db7008498e301dc9c
Philip Ludington <mrphil@mrphilgames.com>
2012-10-06 22:15:04 -0400

    009

--- Assets/Scripts/Targetting.cs
+++ Assets/Scripts/Targetting.cs
@@ -8,13 +8,16 @@ public class Targetting : MonoBehaviour
 
     public Transform selectedTarget;
 
+    private Transform myTransform;
+
     // Use this for initialization
     void Start()
     {
         targets = new List<Transform>();
-        AddAllEnemies();
-
         selectedTarget = null;
+        myTransform = transform;
+
+        AddAllEnemies();
     }
 
     // Update is called once per frame
@@ -41,8 +44,42 @@ public class Targetting : MonoBehaviour
         targets.Add(enemy);
     }
 
+    private void SortTargetsByDistance()
+    {
+        targets.Sort(delegate(Transform t1, Transform t2)
+        {
+            return Vector3.Distance(t1.position, myTransform.position)
+                .CompareTo(Vector3.Distance(t2.position, myTransform.position));
+        });
+    }
+
     private void TargetEnemy()
     {
-        selectedTarget = targets[0];
+        if (selectedTarget == null)
+        {
+            SortTargetsByDistance();
+            selectedTarget = targets[0];
+        }
+        else
+        {
+            int index = targets.IndexOf(selectedTarget);
+
+            if (index < targets.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+
+            selectedTarget = targets[index];
+            SelectTarget();
+        }
+    }
+
+    private void SelectTarget()
+    {
+        selectedTarget.renderer.material.color = Color.red;
     }
 }
